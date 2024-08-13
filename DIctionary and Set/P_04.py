bc41f05b1f209b8b0fe4bc42ff441b6c0aa9f36d
Sarthak001G <sarthakgautam60@gmail.com>
2024-08-13 23:15:13 +0530

    new work to be added

---
+++ DIctionary and Set/P_04.py
@@ -0,0 +1,9 @@
+d ={}
+for i in range(4):
+    name = input('Enter friends name:')
+    lang = input('enter language name:')
+
+    d.update({name:lang})
+print(d)
+
+
---
+++ DIctionary and Set/p_03.py
@@ -0,0 +1,6 @@
+#checking 18 as int and string and how many 18 this set will give
+
+s = set()
+s.add(18)
+s.add("18")
+print (s)
