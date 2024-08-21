44c7d0badf577c6e2dd9deaf93015f28d7849535
Sarthak001G <sarthakgautam60@gmail.com>
2024-08-21 23:34:02 +0530

    added some work

---
+++ Conditional Expression/P_04.py
@@ -0,0 +1,7 @@
+
+username = input("Enter your username:")
+
+if(len(username)<10 ):
+    print("Your username is less than 10")                        
+else:
+    print("Your username length greater or equal to 10")
---
+++ Conditional Expression/P_05.py
@@ -0,0 +1,4 @@
+marks = int(input('Enter your marks'))
+
+if(marks<=100 and marks>=90):
+    print('Ex')
